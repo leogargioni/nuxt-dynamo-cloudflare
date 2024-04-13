206fbc66e0bc452d0e53bfec78e3992b225a2d46
leogargioni <62467812+leogargioni@users.noreply.github.com>
2024-04-13 18:59:46 -0300

    remove wrangler.toml

--- server/api/test1.js
+++ server/api/test1.js
@@ -1,5 +1,14 @@
-
+import { QueryCommand } from "@aws-sdk/client-dynamodb";
 
 export default defineEventHandler(async (event) => {
-    return handleTest1Request()
+    let params = {
+        TableName: process.env.AWS_DYNAMO_TABLE,
+        KeyConditionExpression: "PK = :pk",
+        ExpressionAttributeValues: {
+            ":pk": { S: "GROUP" },
+        },
+    };
+    let data = await ddbClient.send(new QueryCommand(params));
+
+    return data
 });
